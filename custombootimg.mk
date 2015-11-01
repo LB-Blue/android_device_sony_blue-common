e5ddf760b7c5ca1073fa48d4b187bec510e3be08
ISTweak <istweak@d-o-b.org>
2015-11-01 23:24:30 +0900

    fix custombootimg.mk
    Change-Id: Idfb6574e1d41691182b322f8894a68626508846f

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,10 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
+HIJACK_PATH := $(PRODUCT_OUT)/system/bin/hijack
+
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/busybox
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
-	$(hide) cp $(PRODUCT_OUT)/ramdisk.img $(PRODUCT_OUT)/system/bin/hijack/
-	$(hide) cp $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/system/bin/hijack/
+	$(hide) $(MKBOOTFS) $(TARGET_ROOT_OUT) > $(HIJACK_PATH)/ramdisk.cpio
+	$(hide) cp $(PRODUCT_OUT)/utilities/busybox $(HIJACK_PATH)/
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
